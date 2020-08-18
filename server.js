d755c5a947c10e40329996b9bf58946467c97ba9
Sanjeev <a1sanjeev@outlook.com>
2020-08-18 17:37:29 +0530

    change pckg.json

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(methodOverride());
 
 
 router.get('/', function(req, res) {
-    res.sendFile('index.html', { root: __dirname + '/' });
+    res.sendFile('index.html', { root: __dirname + '/dist' });
 });
 
 
